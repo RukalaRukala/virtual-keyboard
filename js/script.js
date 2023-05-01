c6b248a59ac67f4fc7b9ed76714437e39bc27f82
Mikalai Rusakovich <kolyos83@mail.ru>
2023-05-01 17:40:41 +0300

    feat: add animation when pressing buttons from the keyboard

--- js/script.js
+++ js/script.js
@@ -3,28 +3,28 @@ const symbolList = [['`', '~', 'ё', 'Ё', 'tilda', 'let-ё'], ['1', '!', '1', '
 ['5', '%', '5', '%', 'dig-5', 'symb-%'], ['6', '^', '6', ':', 'dig-6', 'symb-^'], ['7', '&', '7', '?', 'dig-7', 'symb-&'],
 ['8', '*', '8', '*', 'dig-8', 'symb-*'], ['9', '(', '9', '(', 'dig-9', 'symb-('], ['0', ')', '0', ')', 'dig-0', 'symb-)'],
 ['-', '_', '-', '_', 'minus', 'symb-_'], ['=', '+', '=', '+', 'plus', 'symb-+'],
-['Backspase', 'Backspase', 'Backspase', 'Backspase', 'backspase black', 'backspase'],
-['Tab', 'Tab', 'Tab', 'Tab', 'tab black', 'tab'], ['q', 'Q', 'й', 'Й', 'let-q', 'let-й'], ['w', 'W', 'ц', 'Ц', 'let-w', 'let-ц'],
+['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace black', 'Backspace'],
+['Tab', 'Tab', 'Tab', 'Tab', 'Tab black', 'Tab'], ['q', 'Q', 'й', 'Й', 'let-q', 'let-й'], ['w', 'W', 'ц', 'Ц', 'let-w', 'let-ц'],
 ['e', 'E', 'у', 'У', 'let-e', 'let-у'], ['r', 'R', 'к', 'К', 'let-r', 'let-к'], ['t', 'T', 'е', 'Е', 'let-t', 'let-е'],
 ['y', 'Y', 'н', 'Н', 'let-y', 'let-е'], ['u', 'U', 'г', 'Г', 'let-u', 'let-г'], ['i', 'I', 'ш', 'Ш', 'let-i', 'let-ш'],
 ['o', 'O', 'щ', 'Щ', 'let-o', 'let-щ'], ['p', 'P', 'з', 'З', 'let-p', 'let-з'], ['[', '{', 'х', 'Х', 'brecket-l', 'let-х'],
-[']', '}', 'ъ', 'Ъ', 'brecket-r', 'let-ъ'], ['\\', '|', '\\', '/', 'reslash', 'let-\\'], ['Del', 'Del', 'Del', 'Del', 'del black', 'del'],
-['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'caps-lock  black', 'caps-lock'], ['a', 'A', 'ф', 'Ф', 'let-a', 'let-ф'],
+[']', '}', 'ъ', 'Ъ', 'brecket-r', 'let-ъ'], ['\\', '|', '\\', '/', 'reslash', 'let-\\'], ['Del', 'Del', 'Del', 'Del', 'del black', 'Delete'],
+['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock black', 'CapsLock'], ['a', 'A', 'ф', 'Ф', 'let-a', 'let-ф'],
 ['s', 'S', 'ы', 'Ы', 'let-s', 'let-Ы'], ['d', 'D', 'в', 'В', 'let-d', 'let-в'], ['f', 'F', 'а', 'А', 'let-f', 'let-а'],
 ['g', 'G', 'п', 'П', 'let-g', 'let-п'], ['h', 'H', 'р', 'Р', 'let-h', 'let-р'], ['j', 'J', 'о', 'О', 'let-j', 'let-о'],
 ['k', 'K', 'л', 'Л', 'let-k', 'let-л'], ['l', 'L', 'д', 'Д', 'let-l', 'let-Д'], [';', ':', 'ж', 'Ж', 'semi', 'let-ж'],
-['\'', '"', 'э', 'Э', 'quotes', 'let-э'], ['Enter', 'Enter', 'Enter', 'Enter', 'enter black', 'enter'],
-['Shift', 'Shift', 'Shift', 'Shift', 'shift shift-l  black', 'shift shift-l'],
+['\'', '"', 'э', 'Э', 'quotes', 'let-э'], ['Enter', 'Enter', 'Enter', 'Enter', 'Enter black', 'Enter'],
+['Shift', 'Shift', 'Shift', 'Shift', 'shift shift-l black', 'Shift'],
 ['z', 'Z', 'я', 'Я', 'let-z', 'let-я'], ['x', 'X', 'ч', 'Ч', 'let-x', 'let-ч'],
 ['c', 'C', 'с', 'С', 'let-c', 'let-с'], ['v', 'V', 'м', 'М', 'let-v', 'let-м'], ['b', 'B', 'и', 'И', 'let-b', 'let-и'],
 ['n', 'N', 'т', 'Т', 'let-n', 'let-т'], ['m', 'M', 'ь', 'Ь', 'let-m', 'let-ь'], [',', '<', 'б', 'Б', 'comma', 'let-б'],
-['.', '>', 'ю', 'Ю', 'dot', 'let-ю'], ['/', '?', '.', ',', 'slash', 'let-.'], ['&#9650;', '&#9650;', '&#9650;', '&#9650;', 'but-up black', 'but-up'],
-['Shift', 'Shift', 'Shift', 'Shift', 'shift shift-r black', 'shift shift-r'],
-['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'ctrl ctrl-l black', 'ctrl ctrl-l'], ['Win', 'Win', 'Win', 'Win', 'win black', 'win'],
-['Alt', 'Alt', 'Alt', 'Alt', 'alt alt-l black', 'alt alt-l'], ['', '', '', '', 'space', 'space'],
-['Alt', 'Alt', 'Alt', 'Alt', 'alt alt-r black', 'alt alt-r'], ['&#9668;', '&#9668;', '&#9668;', '&#9668;', 'but-left  black', 'but-left'],
-['&#9660;', '&#9660;', '&#9660;', '&#9660;', 'but-down black', 'but-down'], ['&#9658;', '&#9658;', '&#9658;', '&#9658;', 'but-right black', 'but-right'],
-['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'ctrl ctrl-r  black', 'ctrl ctrl-r']];
+['.', '>', 'ю', 'Ю', 'dot', 'let-ю'], ['/', '?', '.', ',', 'slash', 'let-.'], ['&#9650;', '&#9650;', '&#9650;', '&#9650;', 'but-up black', 'ArrowUp'],
+['Shift', 'Shift', 'Shift', 'Shift', 'shift shift-r black', 'Shift'],
+['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'ctrl ctrl-l black', 'Control'], ['Win', 'Win', 'Win', 'Win', 'win black', 'Meta'],
+['Alt', 'Alt', 'Alt', 'Alt', 'alt alt-l black', 'Alt'], ['', '', '', '', 'Space', 'Space'],
+['Alt', 'Alt', 'Alt', 'Alt', 'alt alt-r black', 'Alt'], ['&#9668;', '&#9668;', '&#9668;', '&#9668;', 'but-left  black', 'ArrowLeft'],
+['&#9660;', '&#9660;', '&#9660;', '&#9660;', 'but-down black', 'ArrowDown'], ['&#9658;', '&#9658;', '&#9658;', '&#9658;', 'but-right black', 'ArrowRight'],
+['Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'ctrl ctrl-r  black', 'Control']];
 
 const wrapper = document.createElement('div');
 wrapper.setAttribute('class', 'wrapper');
@@ -51,7 +51,7 @@ wrapper.appendChild(textarea);
 wrapper.appendChild(keyboard);
 
 for (let i = 0; i < 64; i += 1) {
-  keyboard.insertAdjacentHTML('beforeend', `<div class="keyboard-but but ${symbolList[i][4]}">
+  keyboard.insertAdjacentHTML('beforeend', `<div id="${symbolList[i][5]}" class="keyboard-but but ${symbolList[i][4]}">
                                                   <span class='eng'>
                                                     <span class='caseDown'>${symbolList[i][0]}</span>
                                                     <span class='caseUp hidden'>${symbolList[i][1]}</span>
@@ -79,9 +79,79 @@ wrapper.appendChild(instuction);
 
 changeRadius = (event) => {
   const name = event.target.className;
-  console.log(name);
-  document.querySelector('.' + name.split(' ')[2]).style.borderRadius = '50px';
+  document.querySelector(`.${name.split(' ')[2]}`).style.borderRadius = '50px';
+  event.stopPropagation();
 };
 
 document.addEventListener('mousedown', changeRadius);
 document.addEventListener('mouseup', (event) => event.target.style.borderRadius = '3px');
+
+// const buttn = document.querySelector('.tilda');
+// buttn.mousedown = function(event) {
+//   event.target.style.borderRadius = '50px';
+// };
+
+// buttn.mouseup = function(event) {
+//   event.target.style.borderRadius = '3px';
+// };
+
+let keys = document.querySelectorAll('.but');
+let spaceKey = document.querySelector('.space');
+let shiftLeft = document.querySelector('.shift-l');
+let shiftRight = document.querySelector('.shift-r');
+let capsLock = document.querySelector('.caps-lock');
+
+for (let i = 0; i < keys.length; i += 1) {
+  keys[i].setAttribute('nameEn', symbolList[i][0]);
+  keys[i].setAttribute('enUpperCase', symbolList[i][1]);
+  keys[i].setAttribute('nameRu', symbolList[i][2]);
+  keys[i].setAttribute('ruUpperCase', symbolList[i][3]);
+}
+
+window.addEventListener('keydown', function(event) {
+  for (let i = 0; i < keys.length; i += 1) {
+    console.log(event.key);
+    if (event.key === keys[i].getAttribute('nameEn')
+    || event.key === keys[i].getAttribute('enUpperCase')
+    || event.key === keys[i].getAttribute('nameRu')
+    || event.key === keys[i].getAttribute('ruUpperCase')) {
+      keys[i].classList.add('active');
+    }
+    if (event.code == 'Space') {
+      spaceKey.classList.add('active');
+    }
+    if (event.code == 'ShiftLeft') {
+      shiftRight.classList.remove('active');
+    }
+    if (event.code == 'ShiftRight') {
+      shiftLeft.classList.remove('active');
+    }
+    if (event.code == 'CapsLock') {
+      CapsLock.classList.toggle('active');
+    }
+  }
+})
+
+window.addEventListener('keyup', function(event) {
+  for (let i = 0; i < keys.length; i += 1) {
+    console.log(event.key);
+    if (event.key === keys[i].getAttribute('nameEn')
+    || event.key === keys[i].getAttribute('enUpperCase')
+    || event.key === keys[i].getAttribute('nameRu')
+    || event.key === keys[i].getAttribute('ruUpperCase')) {
+      keys[i].classList.remove('active');
+      keys[i].classList.add('remove');
+    }
+    if (event.code == 'Space') {
+      spaceKey.classList.remove('active');
+      spaceKey.classList.add('remove');
+    }
+    if (event.code == 'ShiftLeft') {
+      shiftRight.classList.remove('active');
+      spaceKey.classList.remove('remove');
+    }
+    setTimeout(() =>{
+      keys[i].classList.remove('remove')
+    },200)
+  }
+})
